72c2be1b5f06401fa5d59b2d8f6a2791d4ac9cbd
Alex1497 <a01207765@itesm.mx>
2019-02-28 16:47:37 -0600

    Agregar Lab 5

---
+++ lab04/app/src/main/java/mx/tec/lab03/ChildActivity.java
@@ -0,0 +1,113 @@
+package mx.tec.lab03;
+import android.app.Notification;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Activity;
+import android.view.GestureDetector;
+
+public class ChildActivity extends Activity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
+
+    private static final int SWIPE_THRESHOLD = 100;
+
+    private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+    private GestureDetectorCompat mDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        Intent targetIntent;
+        targetIntent = getIntent();
+        String myText;
+        mDetector = new GestureDetectorCompat(this,this);
+
+        mDetector.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        boolean result = false;
+
+        try {
+            float diffY = e2.getY() - e1.getY();
+            float diffX = e2.getX() - e1.getX();
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        //Toast.makeText(getApplicationContext(), "Swipe Right." , Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(ChildActivity.this,MainActivity.class);
+                        startActivity(i);
+                    } else {
+                        //Toast.makeText(getApplicationContext(), "Swipe Left." , Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(ChildActivity.this,ChildActivity2.class);
+                        startActivity(i);
+                    }
+                    result = true;
+                }
+            }
+            else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                if (diffY > 0) {
+                    //Toast.makeText(getApplicationContext(), "Swipe Down." , Toast.LENGTH_SHORT).show();
+                } else {
+                    //Toast.makeText(getApplicationContext(), "Swipe Up." , Toast.LENGTH_SHORT).show();
+                }
+                result = true;
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return result;
+    }
+}
